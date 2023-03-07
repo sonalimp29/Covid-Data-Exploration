5d6a4fcfa3c7c518f9d9da6a0ec2e252bb61dad8
SONALI PATEKAR <56134219+sonalimp29@users.noreply.github.com>
2023-03-07 23:40:01 +0100

    Add files via upload

---
+++ Covid_DataAnalysis_SQL1.sql
@@ -0,0 +1,175 @@
+Select *
+from PortfolioProject..CovidDeaths
+order by 3,4
+
+
+--Select *
+--from PortfolioProject..CovidVaccinations
+--order by 3,4
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+Where continent is not null
+order by 1,2
+
+--Looking at Total Cases vs Total Deaths
+--this shows the likelihood of dying if a person contracts covid in US
+Select Location, date, total_cases, new_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Where location like '%states%'
+order by 1,2
+
+--Looking at total cases vs Population
+--Shows what percentage of population contracted Co
+Select Location, date, total_cases, Population, total_deaths, (total_cases/Population)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Where location like '%states%'
+order by 1,2
+
+
+--Looking at countries with highest infection rate compared to Population
+Select Location, Population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/Population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location, population
+--Where location like '%states%'
+order by PercentPopulationInfected desc
+
+--Countries with Highest Death Count per population
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location
+order by TotalDeathCount desc
+
+
+--LET'S BREAK THINGS BY CONTINENT
+
+select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by continent order by TotalDeathCount desc
+
+
+select location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null
+Group by location 
+order by TotalDeathCount desc
+
+--Showing continents with highest death count per population
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by continent
+order by TotalDeathCount desc
+
+
+-- GLOBAL NUMBERS 
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+order by 1,2
+
+
+--CovidVaccination Data
+Select *
+From PortfolioProject..CovidVaccinations
+
+Select *
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+On dea.location = vac.location
+and dea.date = vac.date
+
+
+--Look at total population vs Vaccination
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+On dea.location = vac.location and dea.date = vac.date
+where dea.continent is not null
+order by 1,2,3
+
+--using window function in above query
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location,
+dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+On dea.location = vac.location and dea.date = vac.date
+where dea.continent is not null
+order by 1,2,3
+
+
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location,
+dea.Date) as RollingPeopleVaccinated-- (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+On dea.location = vac.location and dea.date = vac.date
+where dea.continent is not null
+order by 1,2,3
+
+--USE CTE
+
+with PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location,
+dea.Date) as RollingPeopleVaccinated
+ --(RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+On dea.location = vac.location and dea.date = vac.date
+where dea.continent is not null
+order by 1,2,3
+)
+select*,(RolligPeopleVaccinated/Population)*100
+From PopvsVac
+
+
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+--where dea.continent is not null 
+--order by 2,3
+
+Select *, (RollingPeopleVaccinated/Population)*100
+From #PercentPopulationVaccinated
+
+
+
+
+-- Creating View to store data for later visualizations
+
+Create View PercentPopulationVaccinated as 
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location,
+dea.Date) as RollingPeopleVaccinationed
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+On dea.location = vac.location and 
+dea.date = vac.date
+Where dea.continent is not null
